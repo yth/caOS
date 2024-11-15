53fd640cd31b281aef3cba70492e46ca8c193497
unknown <ca132731@gmail.com>
2024-11-15 12:22:02 -0500

    my commit

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,8 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
+static HELLO: &[u8] = b"Hello World!";
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     let vga_buffer = 0xb8000 as *mut u8;
